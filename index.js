0f52bd4c580c38711aaaef910cc39d0547529779
Deborah Hardy <deb_hardy@me.com>
2018-10-03 20:24:51 -0700

    variables

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello bcpreppers!");
+var x = 12;
+
+if (x % 2 === 0) {
+    console.log ("is even")
+}
+
+else {
+    console.log ("is odd");
+}
+
+//(init; condition; post-loop)
+for(var i = 0; i < 100; i++) {
+    var y = i * 2;
+    if(y < 101) {
+    console.log("the number is", y)
+    }
+}
